aa69068e0127510275576a4df94e988541204c87
Benoit Gambier <contact@nouvelle-techno.fr>
2019-11-30 13:39:45 +0100

    Initial commit

---
+++ paiement.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Paiement</title>
+    </head>
+    <body>
+        
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
